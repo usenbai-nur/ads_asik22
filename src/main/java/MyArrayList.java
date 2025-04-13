f27d42b4a5a8500feee66eb50f3a2d5f3fddf575
assignment <usenbainurdaulet1@gmail.com>
2025-04-13 15:38:26 +0500

    .

---
+++ src/main/java/MyArrayList.java
@@ -0,0 +1,138 @@
+public class MyArrayList<T> implements MyList<T> {
+    private Object[] data;
+    private int size;
+
+    public MyArrayList() {
+        data = new Object[10];
+        size = 0;
+    }
+
+    private void ensureCapacity() {
+        if (size >= data.length) {
+            Object[] newData = new Object[data.length * 2];
+            System.arraycopy(data, 0, newData, 0, data.length);
+            data = newData;
+        }
+    }
+
+    public void add(T item) {
+        ensureCapacity();
+        data[size++] = item;
+    }
+
+    public void set(int index, T item) {
+        checkIndex(index);
+        data[index] = item;
+    }
+
+    public void add(int index, T item) {
+        checkIndexForAdd(index);
+        ensureCapacity();
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = item;
+        size++;
+    }
+
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    public void addLast(T item) {
+        add(item);
+    }
+
+    public T get(int index) {
+        checkIndex(index);
+        return (T) data[index];
+    }
+
+    public T getFirst() {
+        return get(0);
+    }
+
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    public void remove(int index) {
+        checkIndex(index);
+        System.arraycopy(data, index + 1, data, index, size - index - 1);
+        data[--size] = null;
+    }
+
+    public void removeFirst() {
+        remove(0);
+    }
+
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    public void sort() {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                Comparable<T> a = (Comparable<T>) data[i];
+                if (a.compareTo((T) data[j]) > 0) {
+                    Object temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+        }
+    }
+
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (data[i].equals(object)) return i;
+        }
+        return -1;
+    }
+
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    public Object[] toArray() {
+        Object[] arr = new Object[size];
+        System.arraycopy(data, 0, arr, 0, size);
+        return arr;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) data[i] = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+    }
+
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            int cursor = 0;
+
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            public T next() {
+                return (T) data[cursor++];
+            }
+        };
+    }
+}
